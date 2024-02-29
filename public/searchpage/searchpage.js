f6eadc3d2ededd43a1a567200de8c3d263bf13a0
B251 <ba.sadler1@gmail.com>
2024-02-29 20:13:24 +0000

    created script for creating custom URL for job search API

--- public/searchpage/searchpage.js
+++ public/searchpage/searchpage.js
@@ -1,34 +1,37 @@
-
+var basicAddress;
+var minsalary;
+var maxsalary;
 var resultsperpage = 20
 var apiKey = "e6b644fb61f2e5d8cb060935d76de379";
 var appID = "1cad4d53";
 var APIURL;
 
-function searchResults() {
-  let minsalary
-  let maxsalary
-  let jobTitle = document.getElementById("jobTitle").value;
-  let location = document.getElementById("location").value;
-  let salary = document.getElementById("salary").value;
-  let jobType = document.getElementById("jobType").value;
-  let hours = document.getElementById("hours").value;
-  salarySort(salary);
-  APIURL = customURL(jobTitle, location, minsalary, maxsalary, jobType, hours);
-  console.log (APIURL)
-}
-
+/* Working */
 function salarySort(salaryrange) {
   let salaryArr = salaryrange.split("-");
   minsalary = salaryArr[0];
   maxsalary = salaryArr[1];
 }
 
+function searchResults() {
+  let jobTitle = document.getElementById("jobTitle").value.trim();
+  let location = document.getElementById("location").value.trim();
+  let salary = document.getElementById("salary").value;
+  let jobType = document.getElementById("jobType").value;
+  let hours = document.getElementById("hours").value;
+  salarySort(salary);
+  APIURL = customURL(jobTitle, location, minsalary, maxsalary, jobType, hours);
+  console.log (APIURL);
+}
+
  function jobTypeCheck(basicAddress, jobType) {
   if (jobType === "Permanent") {
-    basicAddress.concat("&permanent=1");
+    basicAddress = basicAddress.concat("&permanent=1");
+    return basicAddress
   }
   else if (jobType === "Contract") {
-    basicAddress.concat("&contract=1");
+    basicAddress = basicAddress.concat("&contract=1");
+    return basicAddress
   }
   else {
     return basicAddress
@@ -37,11 +40,11 @@ function salarySort(salaryrange) {
  
 function hoursType(basicAddress, hours) {
   if (hours === "PartTime") {
-    basicAddress.concat("&part_time=1");
+    basicAddress = basicAddress.concat("&part_time=1");
     return basicAddress
   }
   else if (hours === "FullTime") {
-    basicAddress.concat("&full_time=1");
+    basicAddress = basicAddress.concat("&full_time=1");
     return basicAddress
   }
   else {
@@ -50,16 +53,8 @@ function hoursType(basicAddress, hours) {
 }
 
 function customURL(jobTitle, location, minsalary, maxsalary, jobType, hours) {
-  let basicAddress = `https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${appID}&app_key=${apiKey}&results_per_page=${resultsperpage}&what=${jobTitle}&where=${location}&salary_min=${minsalary}&salary_max=${maxsalary}`
+  basicAddress = `https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${appID}&app_key=${apiKey}&results_per_page=${resultsperpage}&what=${jobTitle}&where=${location}&salary_min=${minsalary}&salary_max=${maxsalary}`
   basicAddress = jobTypeCheck(basicAddress, jobType);
   basicAddress = hoursType(basicAddress, hours);
+  return basicAddress;
 }
-
-
-/* if condition met append dto URL, else append something else.*/
-
-/* https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=1cad4d53&app_key=e6b644fb61f2e5d8cb060935d76de379&results_per_page=20&what=Developer&where=London&salary_min=20000&salary_max=30000&full_time=1&contract=1 */
-
-/* https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=1cad4d53&app_key=e6b644fb61f2e5d8cb060935d76de379&results_per_page=20&what=Developer&where=London&salary_min=20000&salary_max=30000&part_time=1&permanent=1 */
-
-/* https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=1cad4d53&app_key=e6b644fb61f2e5d8cb060935d76de379&results_per_page=20&what=Developer&where=London&salary_min=20000&salary_max=30000 */
