68c642abcac635a6772bb89e16dafb76fed16de0
Michael Ergette <michael_e96@hotmail.co.uk>
2024-02-29 19:27:41 +0000

    useffect fetch api

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,18 @@
 import { useEffect, useState } from "react";
+export default function Sample() {
+    useEffect(() => {
+        fetch("https://type.fit/api/quotes")
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            setSample(data);
+            console.log(sample);
+          });
+      }, []);
+
+
+
 
 const Home = () => {
   return <h1>HomePage</h1>;
