951383e5399202cd175a380334ccbe35805ab28e
Michael Ergette <michael_e96@hotmail.co.uk>
2024-03-01 18:28:21 +0000

    added code for local storage and fixed jobcard error

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react";
+import JobCard from "./../components/JobCard";
+import { json } from "react-router-dom";
+
+
 export default function Home() {
-    const [randomQuote, setRandomQuote] = useState(null);
+  const [randomQuote, setRandomQuote] = useState(null);
   const [quoteList, setquoteList] = useState([]);
   useEffect(() => {
+
+    const jobList =JSON.parse(localStorage.getItem("jobData"))
+    console.log(jobList)
     fetch("https://type.fit/api/quotes")
+    //useffect fetches the data once
       .then(function (response) {
         return response.json();
       })
@@ -11,22 +19,24 @@ export default function Home() {
         setquoteList(data);
         console.log(data);
         let randomIndex = Math.floor(Math.random() * data.length);
-      //  setRandomQuote(data[randomIndex]);
-      let quote = data[randomIndex];
-      quote.author = quote.author.replace(', type.fit', ' ');
-      setRandomQuote(quote);
-
+        //  setRandomQuote(data[randomIndex]);
+        let quote = data[randomIndex];
+        quote.author = quote.author.replace(", type.fit", " ");
+        setRandomQuote(quote);
       });
   }, []);
-  return (
+  return ( 
     <div>
       <h1>HomePage</h1>
-
+      <JobCard />;
       {randomQuote && (
-          <div>
-            {randomQuote.text} - {randomQuote.author}
-          </div>
-        )}
+        <div className="text-primary"> 
+          {randomQuote.text} - {randomQuote.author}
+        </div>
+      )}
+
+{/* map through the local strorage
+for each item we pass it as a prop throught the job card */}
 
       {/* <p>
         {sample.map((item, index) => (
@@ -36,9 +46,6 @@ export default function Home() {
     </div>
   );
 }
-
-
-
 
 // const Home = () => {
 //   return <h1>HomePage</h1>;
--- src/Pages/JobForm.js
+++ src/Pages/JobForm.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
-import styles from './JobForm.css';
+import React, { useState } from "react";
+import styles from "./JobForm.css";
 
 function JobForm({ onJobSubmit }) {
   const [jobData, setJobData] = useState({
-    companyName: '',
-    jobTitle: '',
-    description: '',
-    location: '',
-    salary: '',
-    jobType: '',
-    employmentType: '',
-    linkToAd: '',
-    jobStatus: '', 
-    additionalNotes: ''
+    //creating an object for jobdata with the following key:values
+    //setjobdata to update the variable we use setjobdata based on user input
+    companyName: "",
+    jobTitle: "",
+    description: "",
+    location: "",
+    salary: "",
+    jobType: "",
+    employmentType: "",
+    linkToAd: "",
+    jobStatus: "",
+    additionalNotes: "",
   });
 
   const handleChange = (e) => {
@@ -21,29 +23,64 @@ function JobForm({ onJobSubmit }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onJobSubmit(jobData);
+    //onJobSubmit(jobData);
+    // Reset form after submission
+    let allJobData = JSON.parse(localStorage.getItem("jobData")) || []
+    allJobData.push(jobData)
+    localStorage.setItem("jobData", JSON.stringify(allJobData));
     setJobData({
-      companyName: '',
-      jobTitle: '',
-      description: '',
-      location: '',
-      salary: '',
-      jobType: '',
-      employmentType: '',
-      linkToAd: '',
-      jobStatus: '',
-      additionalNotes: ''
-    }); // Reset form after submission
-    localStorage.setItem("Job data",JSON.stringify(jobData))
+      companyName: "",
+      jobTitle: "",
+      description: "",
+      location: "",
+      salary: "",
+      jobType: "",
+      employmentType: "",
+      linkToAd: "",
+      jobStatus: "",
+      additionalNotes: "",
+    });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="companyName" value={jobData.companyName} onChange={handleChange} placeholder="Company Name" required />
-      <input type="text" name="jobTitle" value={jobData.jobTitle} onChange={handleChange} placeholder="Job Title" required />
-      <textarea name="description" value={jobData.description} onChange={handleChange} placeholder="Job Description" required></textarea>
-      <input type="text" name="location" value={jobData.location} onChange={handleChange} placeholder="Location" required />
-      <select name="salary" value={jobData.salary} onChange={handleChange} required>
+      <input
+        type="text"
+        name="companyName"
+        value={jobData.companyName}
+        onChange={handleChange}
+        placeholder="Company Name"
+        required
+      />
+      <input
+        type="text"
+        name="jobTitle"
+        value={jobData.jobTitle}
+        onChange={handleChange}
+        placeholder="Job Title"
+        required
+      />
+      <textarea
+        name="description"
+        value={jobData.description}
+        onChange={handleChange}
+        placeholder="Job Description"
+        required
+      ></textarea>
+      <input
+        type="text"
+        name="location"
+        value={jobData.location}
+        onChange={handleChange}
+        placeholder="Location"
+        required
+      />
+      <select
+        name="salary"
+        value={jobData.salary}
+        onChange={handleChange}
+        required
+      >
         <option value="below £20,000">Below £20,000</option>
         <option value="£20,000-29,999">£20,000-29,999</option>
         <option value="£30,000-39,999">£30,000-39,999</option>
@@ -51,27 +88,53 @@ function JobForm({ onJobSubmit }) {
         <option value="£50,000-59,999">£50,000-59,999</option>
         <option value="above £60,000">Above £60,000</option>
       </select>
-      <select name="jobType" value={jobData.jobType} onChange={handleChange} required>
+      <select
+        name="jobType"
+        value={jobData.jobType}
+        onChange={handleChange}
+        required
+      >
         <option value="remote">Remote</option>
         <option value="on-site">On-Site</option>
         <option value="hybrid">Hybrid</option>
       </select>
-      <select name="employmentType" value={jobData.employmentType} onChange={handleChange} required>
+      <select
+        name="employmentType"
+        value={jobData.employmentType}
+        onChange={handleChange}
+        required
+      >
         <option value="full-time">Full-Time</option>
         <option value="part-time">Part-Time</option>
         <option value="contract">Contract</option>
         <option value="temporary">Temporary</option>
         <option value="internship">Internship</option>
       </select>
-      <select name="jobStatus" value={jobData.jobStatus} onChange={handleChange} required>
+      <select
+        name="jobStatus"
+        value={jobData.jobStatus}
+        onChange={handleChange}
+        required
+      >
         <option value="interested">Interested</option>
         <option value="applied">Applied</option>
         <option value="interview">Interview Stage</option>
         <option value="offered">Offered</option>
         <option value="offered">Rejected</option>
       </select>
-      <input type="url" name="linkToAd" value={jobData.linkToAd} onChange={handleChange} placeholder="Link to Job Advert/ Application Portal" />
-      <textarea name="additionalNotes" value={jobData.additionalNotes} onChange={handleChange} placeholder="Additional Notes"></textarea>
+      <input
+        type="url"
+        name="linkToAd"
+        value={jobData.linkToAd}
+        onChange={handleChange}
+        placeholder="Link to Job Advert/ Application Portal"
+      />
+      <textarea
+        name="additionalNotes"
+        value={jobData.additionalNotes}
+        onChange={handleChange}
+        placeholder="Additional Notes"
+      ></textarea>
       <button type="submit">Add Job!</button>
     </form>
   );
