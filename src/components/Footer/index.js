6b96ff2ea00f34da1aec7e15f62a0da86a671dbc
Lucas Zhao <Lucaszhaopeiqi@hotmail.com>
2024-03-04 19:45:52 +0000

    adjusted logo at footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,15 +4,13 @@ import { FaGithub } from 'react-icons/fa';
 
 function Footer() {
     return (
-      <footer className="bg-dark text-white pt-4 pb-2 mt-4">
+        <footer className="bg-dark text-white pt-4 pb-2 mt-4">
         <Container>
-          <Row className="justify-content-center mb-2">
+          <Row className="justify-content-center mb-2 align-items-center"> 
             <Col xs="auto">
-              <img src={`${process.env.PUBLIC_URL}/images/logo3.webp`} alt="JobFlow Logo" style={{ height: '50px' }} />
+              <img src={`${process.env.PUBLIC_URL}/images/logo3.webp`} alt="JobFlow Logo" style={{ height: '60px' }} />
             </Col>
-          </Row>
-          <Row>
-            <Col className="d-flex justify-content-center">
+            <Col className="d-flex justify-content-center"> 
               <a href="https://github.com/zhaolucas" className="text-white mx-2"><FaGithub size={24} /> Lucas </a>
               <a href="https://github.com/BenA251" className="text-white mx-2"><FaGithub size={24} /> Ben </a>
               <a href="https://github.com/SophieEbby" className="text-white mx-2"><FaGithub size={24} /> Sophie </a>
@@ -22,6 +20,7 @@ function Footer() {
           </Row>
         </Container>
       </footer>
+      
     );
   }
   
