3bb7fa1d7aea23db19d3c0b8048de23b92412b12
Lucas Zhao <Lucaszhaopeiqi@hotmail.com>
2024-03-04 19:57:12 +0000

    added styling for footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { FaGithub } from 'react-icons/fa';
+import style from './style.css';
 
 function Footer() {
     return (
-        <footer className="bg-dark text-white pt-4 pb-2 mt-4">
+      <footer className="footer">
         <Container>
-          <Row className="justify-content-center mb-2 align-items-center"> 
+          <Row className="justify-content-center align-items-center">
             <Col xs="auto">
-              <img src={`${process.env.PUBLIC_URL}/images/logo3.webp`} alt="JobFlow Logo" style={{ height: '60px' }} />
+              <img src={`${process.env.PUBLIC_URL}/images/logo3.webp`} alt="JobFlow Logo" className="footer-logo" />
             </Col>
-            <Col className="d-flex justify-content-center"> 
-              <a href="https://github.com/zhaolucas" className="text-white mx-2"><FaGithub size={24} /> Lucas </a>
-              <a href="https://github.com/BenA251" className="text-white mx-2"><FaGithub size={24} /> Ben </a>
-              <a href="https://github.com/SophieEbby" className="text-white mx-2"><FaGithub size={24} /> Sophie </a>
-              <a href="https://github.com/englburc" className="text-white mx-2"><FaGithub size={24} /> Levan </a>
-              <a href="https://github.com/michaelerg" className="text-white mx-2"><FaGithub size={24} /> Michael </a>
+            <Col className="text-center">
+              <div className="d-flex justify-content-center">
+                <a href="https://github.com/zhaolucas" className="footer-link mx-2"><FaGithub size={40} /> lucas </a>
+                <a href="https://github.com/BenA251" className="footer-link mx-2"><FaGithub size={40} /> ben </a>
+                <a href="https://github.com/SophieEbby" className="footer-link mx-2"><FaGithub size={40} /> sophie </a>
+                <a href="https://github.com/englburc" className="footer-link mx-2"><FaGithub size={40} /> levan </a>
+                <a href="https://github.com/michaelerg" className="footer-link mx-2"><FaGithub size={40} /> michael </a>
+              </div>
             </Col>
           </Row>
         </Container>
       </footer>
-      
     );
   }
   
